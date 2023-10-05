8ceffa5140a023a0dfd0b7ad7d2074b4b225b3d6
Denis <den.sautner@mail.ru>
2023-10-05 21:49:08 +0600

    finished posts-page-component

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 // Замени на свой, чтобы получить независимый от других набор данных.
 // "боевая" версия инстапро лежит в ключе prod
-const personalKey = "prod";
+const personalKey = "sautner-denis";
+const defaultKey = "prod"
 const baseHost = "https://webdev-hw-api.vercel.app";
-const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
+const postsHost = `${baseHost}/api/v1/${defaultKey}/instapro`;
 
 export function getPosts({ token }) {
   return fetch(postsHost, {
