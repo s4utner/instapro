f7c21c5f2e4004af9503e934b4c8cc29c8b190fa
Denis <den.sautner@mail.ru>
2023-10-07 19:53:16 +0600

    refactoring user-posts-page-component

--- components/user-posts-page-component.js
+++ components/user-posts-page-component.js
@@ -1,6 +1,6 @@
 import { USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage, renderApp } from "../index.js";
+import { posts, goToPage } from "../index.js";
 import { likeEventListener, likedUsers } from "./like-component.js";
 
 export function renderUserPostsPageComponent({ appEl }) {
